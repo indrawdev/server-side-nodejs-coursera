5ee9569355c5760691c8fe945226d17243677031
Indra Pramana <indra@ide.web.id>
2021-02-18 14:39:01 +0700

    Mongoose Part 1

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
 
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
@@ -17,6 +23,35 @@ app.use('/promos', promoRouter);
 app.use('/leaders', leaderRouter);
 
 const server = http.createServer(app);
+
+connect.then((db) => {
+
+	console.log('Connected correctly to server');
+
+	var newDish = Dishes({
+		name: 'Uthappizza',
+		description: 'test'
+	});
+
+	newDish.save()
+		.then((dish) => {
+			console.log(dish);
+
+			return Dishes.find({});
+		})
+		.then((dishes) => {
+			console.log(dishes);
+
+			return Dishes.remove({});
+		})
+		.then(() => {
+			return mongoose.connection.close();
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+
+});
 
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
